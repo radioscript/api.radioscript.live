bff3e4bed92f575238d87b77f84b7b4321b94e9e
Ali <asafari72@gmail.com>
2025-07-28 22:56:58 +0330

    Apply role-based access control to all methods in TagController, ensuring only authorized users can create, update, recover, and delete tags.

--- features/tags/src/lib/tags.controller.ts
+++ features/tags/src/lib/tags.controller.ts
@@ -5,12 +5,12 @@ import { JwtAuthGuard, RolesGuard } from '@/guards';
 import { Body, Controller, Delete, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { TagService } from './tags.service';
 
-@UseGuards(JwtAuthGuard, RolesGuard)
-@Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
 @Controller('tags')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
   @Post()
   create(@Body() body: CreateTagDto) {
     return this.tagService.create(body.name, body.slug);
@@ -26,16 +26,20 @@ export class TagController {
     return this.tagService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
   @Put('recover/:id')
   recover(@Param('id') id: string) {
     return this.tagService.recover(id);
   }
-
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
   @Get('deleted')
   deleted() {
     return this.tagService.deleted();
   }
-
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.tagService.remove(id);
