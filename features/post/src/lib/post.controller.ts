d1755b152e1096da5d0938cdb9af120e5a4d8b2b
Ali <asafari72@gmail.com>
2025-07-28 22:55:03 +0330

    Enhance PostController with role-based access control for findAll and findOne methods, ensuring consistent security across all endpoints.

--- features/post/src/lib/post.controller.ts
+++ features/post/src/lib/post.controller.ts
@@ -7,35 +7,41 @@ import { Body, ClassSerializerInterceptor, Controller, Delete, Get, Param, Patch
 import { Request } from 'express';
 import { PostService } from './post.service';
 
-@UseGuards(JwtAuthGuard, RolesGuard)
 @UseInterceptors(ClassSerializerInterceptor)
 @Controller('post')
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
   @Post()
   create(@Req() req: Request, @Body() dto: CreatePostDto) {
     return this.postService.create(req, dto);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
   @Get()
   findAll(@Query() query: PostQueryDto) {
     return this.postService.findAll(query);
   }
 
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.postService.findOne(id);
   }
 
   @Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: UpdatePostDto) {
     return this.postService.update(id, dto);
   }
 
   @Roles(UserRole.ADMIN, UserRole.SUPER_ADMIN)
+  @UseGuards(JwtAuthGuard, RolesGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.postService.remove(id);
