8d0b8722cfd052f0d915a4fef1c2d2f8f188be38
Ali <asafari72@gmail.com>
2025-07-29 23:23:17 +0330

    Update nx-rules.mdc for clarity and add TokenModule to CommentModule imports

--- features/comments/src/lib/comments.module.ts
+++ features/comments/src/lib/comments.module.ts
@@ -1,11 +1,12 @@
 import { Comment, Post, User } from '@/entities';
+import { TokenModule } from '@/token';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommentController } from './comments.controller';
 import { CommentService } from './comments.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Comment, Post, User])],
+  imports: [TypeOrmModule.forFeature([Comment, Post, User]), TokenModule],
   controllers: [CommentController],
   providers: [CommentService],
   exports: [CommentService],
