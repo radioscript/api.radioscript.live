b3e33f5c8e921f9115e9ee1a99b93730b501ff02
Ali <asafari72@gmail.com>
2025-08-20 02:14:23 +0330

    Update Donations Module to Include Role and Permission Entities
    - Enhanced the DonationsModule by adding Role and Permission entities to the TypeOrmModule imports.
    - Updated module configuration to support new functionalities related to user roles and permissions.

--- features/donations/src/lib/donations.module.ts
+++ features/donations/src/lib/donations.module.ts
@@ -1,11 +1,11 @@
-import { Donation, User } from '@/entities';
+import { Donation, Permission, Role, User } from '@/entities';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DonationsController } from './donations.controller';
 import { DonationsService } from './donations.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Donation, User])],
+  imports: [TypeOrmModule.forFeature([Donation, User, Role, Permission])],
   controllers: [DonationsController],
   providers: [DonationsService],
   exports: [DonationsService],
